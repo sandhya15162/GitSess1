7ef1570af1819bd74cd9a2e8edb0c00bde5c974d
sandy <sandhyarani.dalli@gmail.com>
2023-04-26 17:34:40 +0530

    2nd commit fron session1

--- src/test/java/divishaAcademy/demo1.java
+++ src/test/java/divishaAcademy/demo1.java
@@ -4,6 +4,7 @@ public class demo1 {
 
 	public static void main(String[] args) {
 		System.out.println("hello update from GitSession");
+		System.out.println("2nd comment from GitSession1");
 
 	}
 
